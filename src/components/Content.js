bde522ead6eb4cc0920b00f34d00db4933ffff2b
Pawliks <pawliks.ph@gmail.com>
2022-10-12 09:45:39 +0200

    add MainArticle.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,9 @@
+import MainArticle from "./MainArticle";
+
 const Content = () => {
     return ( 
         <main>
-            <article>
-                <h1>Wypełnij test i porównaj swoje poglądy</h1>
-                <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
-                <button className="btn btn-secondary">WYPEŁNIJ TEST</button>
-                <button className="btn btn-outline-secondary">O PROJEKCIE</button>
-            </article>
+            <MainArticle />
         </main>
     );
 }
---
+++ src/components/MainArticle.js
@@ -0,0 +1,12 @@
+const MainArticle = () => {
+    return ( 
+        <article>
+                <h2>Wypełnij test i porównaj swoje poglądy</h2>
+                <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
+                <button className="btn btn-secondary">WYPEŁNIJ TEST</button>
+                <button className="btn btn-outline-secondary">O PROJEKCIE</button>
+        </article>
+    );
+}
+ 
+export default MainArticle;
