306cf45db393e0e26686cff46c7b3e3440bf2392
Pawliks <pawliks.ph@gmail.com>
2022-10-12 10:24:31 +0200

    change about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 const About = () => {
     return ( 
-        <article>
-            <h2>JAK DZIAŁA APLIKACJA?</h2>
-        </article>
+            <article>
+                <h2>JAK DZIAŁA APLIKACJA?</h2>
+                <p>
+                    Przygotowujemy zestaw pytań
+                </p>
+                <p>
+                    Porównujemy twoje poglądy
+                </p>
+                <p>
+                    Wyświetlamy twoje dopasowania
+                </p>
+            </article>
     );
 }
  
