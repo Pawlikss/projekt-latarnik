e406f096559a641bd161952efc445d9beb9eb616
Pawliks <pawliks.ph@gmail.com>
2022-10-12 09:28:25 +0200

    add content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Content from './components/Content';
 
 function App() {
   return (
     <>
-      <Header />  
+      <Header />
+      <Content />
     </>
   );
 }
