a81ad4b170eda3c18fddd91052de8121e2a0d55f
Pawliks <pawliks.ph@gmail.com>
2022-10-12 10:02:49 +0200

    add nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,16 @@
 const Navigation = () => {
     return ( 
-        <nav>
-            
-        </nav>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link" href="#">O PROJEKCIE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">KONTAKT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">WYPEŁNIJ TEST</a>
+            </li>
+        </ul>
     );
 }
  
