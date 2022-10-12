f62632bb0c56adf0d0dc4852c74a8366e17434fb
Pawliks <pawliks.ph@gmail.com>
2022-10-12 11:18:47 +0200

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import Navigation from "./Navigation";
+
+const Footer = () => {
+    return ( 
+        <footer>
+            <Navigation />
+        </footer>
+    );
+}
+ 
+export default Footer;
