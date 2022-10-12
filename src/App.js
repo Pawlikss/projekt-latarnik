8dc3481ddad8834a79c9016f253dfbaf11868d34
Pawliks <pawliks.ph@gmail.com>
2022-10-12 09:38:28 +0200

    modify content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,8 @@ const Content = () => {
             <article>
                 <h1>Wypełnij test i porównaj swoje poglądy</h1>
                 <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
+                <button className="btn btn-secondary">WYPEŁNIJ TEST</button>
+                <button className="btn btn-outline-secondary">O PROJEKCIE</button>
             </article>
         </main>
     );
