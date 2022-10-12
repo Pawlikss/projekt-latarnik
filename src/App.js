f80440508389b5d608b711b4bd882c9c58b78376
Pawliks <pawliks.ph@gmail.com>
2022-10-12 11:19:25 +0200

    add Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 }
