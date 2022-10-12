a2459d32b280e977f3f75d6ab50f0954661f67fc
Pawliks <pawliks.ph@gmail.com>
2022-10-12 09:19:28 +0200

    change content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,10 @@
 const Content = () => {
     return ( 
         <main>
-            
+            <article>
+                <h1>Wypełnij test i porównaj swoje poglądy</h1>
+                <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
+            </article>
         </main>
     );
 }
