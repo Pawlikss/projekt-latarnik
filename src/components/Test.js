7cfd5d0a3996926311f2e679777346052dba28f5
Pawliks <pawliks.ph@gmail.com>
2022-10-13 15:54:52 +0200

    d

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,49 +1,37 @@
-import "../index.css";
-import { motion, useMotionValue, useTransform, useAnimation } from "framer";
+import Card from "./Card"
 
-const style = {
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "contain",
-    backgroundColor: "#55ccff",
-    boxShadow: "5px 10px 18px #888888",
-    borderRadius: 10,
-    height: 300,
-    width: 300,
-    textAlign: "center",
-  };
+import "../index.css";
 
 const Test = () => {
-  const motionValue = useMotionValue(0);
-  
-  const rotateValue = useTransform(motionValue, [-200, 200], [-50, 50]);
-  
-  const opacityValue = useTransform(
-    motionValue,
-    [-200, -150, 0, 150, 200],
-    [0, 1, 1, 1, 0]
-  );
-  
-  const animControls = useAnimation();
+  const cards = [
+    {
+      text: 'Ustawa 1',
+      color: '#55ccff'
+    },
+    {
+      text: 'Ustawa 2',
+      color: '#e8e8e8'
+    },
+    {
+      text: 'Ustawa 3',
+      color: '#0a043c'
+    },
+    {
+      text: 'Ustawa 4',
+      color: 'black'
+    }
+  ];
   
   return (
-    <center>
-      <motion.div
-        center
-        drag="x"
-        x={motionValue}
-        rotate={rotateValue}
-        opacity={opacityValue}
-        dragConstraints={{ left: -1000, right: 1000 }}
-        style={style}
-        onDragEnd={(event, info) => {
-          if (Math.abs(info.point.x) <= 150) {
-            animControls.start({ x: 0 });
-          } else {
-            animControls.start({ x: info.point.x < 0 ? -200 : 200 });
-          }
-        }}
-      />
-    </center>
+    <div className='App'>
+      
+      {/* Traversing through cards array using map function
+      and populating card with different image and color */}
+        
+      {cards.map((card) => (
+        <Card text={card.text} color={card.color}></Card>
+      ))}
+    </div>
   );
 }
  
