54f87efaa9bda0879c9af5e0941f91b5b69bb700
dawidoczek <rej.dawido@gmail.com>
2022-10-17 00:28:12 +0200

    CHANGED BACK LINKING TO THE BATTONS(FUCK WHOEVER REMOVED IT) and +150px

--- src/components/MainArticle.js
+++ src/components/MainArticle.js
@@ -4,8 +4,8 @@ const MainArticle = () => {
         <article className="center">
             <h2>Wypełnij test i porównaj swoje poglądy</h2>
             <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
-            <button className="button1">Wypełnij test</button>
-            <button className="button2">O Projekcie</button>
+            <a href="test"><button className="button1">Wypełnij test</button></a>
+            <a href="test2"><button className="button2">O Projekcie(testowanie bazy)</button></a>
             <img src="seo_illustrations_7.png" alt="Ilustracja monitora" width="20%" height="20%" />
         </article>
     );
