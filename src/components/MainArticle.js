24adf0ab16a69723bbd125dd363078a2e401e059
dawidoczek <rej.dawido@gmail.com>
2022-10-17 01:15:42 +0200

    fixed link issues (fuck whoever got rid of it)

--- src/components/MainArticle.js
+++ src/components/MainArticle.js
@@ -1,11 +1,12 @@
 import styles from '../styl.css'
+import { Link } from "react-router-dom";
 const MainArticle = () => {
     return (
         <article className="center">
             <h2>Wypełnij test i porównaj swoje poglądy</h2>
             <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
-            <a href="/test"><button className="button1">Wypełnij test</button></a>
-            <a href="/test2"><button className="button2">O Projekcie(testowanie bazy)</button></a>
+            <Link to="/test"><button className="button1">Wypełnij test</button></Link>
+            <Link to="/test2"><button className="button2">O Projekcie(testowanie bazy)</button></Link>
             <img src="seo_illustrations_7.png" alt="Ilustracja monitora" width="20%" height="20%" />
         </article>
     );
