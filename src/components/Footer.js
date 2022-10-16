d3d974e3823a8b357cafa22e7db9889cb58ec25c
KsawierCz <ksawiercz1222@gmail.com>
2022-10-16 22:00:53 +0200

    drobny update stopki

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from '../styl.css'
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="footerText">Strona została utworzona przez uczniów klasy 3ct technikum Energetyczno-Informatycznego w Lublinie</p>
+            <p className="footerText">Strona została utworzona przez uczniów z Lublina</p>
         </footer>
     );
 }
