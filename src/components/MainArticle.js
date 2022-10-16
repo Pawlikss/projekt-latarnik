cdb2527e804c3006592ae14f4d3111444a6beda3
dawidoczek <rej.dawido@gmail.com>
2022-10-17 00:49:31 +0200

    modified the link hope it works now xoxo

--- src/components/MainArticle.js
+++ src/components/MainArticle.js
@@ -4,8 +4,8 @@ const MainArticle = () => {
         <article className="center">
             <h2>Wypełnij test i porównaj swoje poglądy</h2>
             <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
-            <a href="test"><button className="button1">Wypełnij test</button></a>
-            <a href="test2"><button className="button2">O Projekcie(testowanie bazy)</button></a>
+            <a href="/test"><button className="button1">Wypełnij test</button></a>
+            <a href="/test2"><button className="button2">O Projekcie(testowanie bazy)</button></a>
             <img src="seo_illustrations_7.png" alt="Ilustracja monitora" width="20%" height="20%" />
         </article>
     );
