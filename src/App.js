2af37324447a2e9dd9068304019ade0c56f8fd6a
dawidoczek <rej.dawido@gmail.com>
2022-10-16 23:56:59 +0200

    Added new testing file, added firebase(check if it works)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Test from "./components/Test";
+import Test2 from "./components/test2";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/test" element={<Test/>}/>
+        <Route path="/test2" element={<Test2/>}/>
       </Routes>
     </Router>
   );
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,22 +1,86 @@
 import Card from "./Card"
-
+import {useState} from 'react';
 import "../index.css";
+import {getDatabase, ref,onValue, child, get } from "firebase/database";
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzKjRaCbrubxYofXuH_PlZ8HD0ye9GRDc",
+  authDomain: "tinder-polityczny.firebaseapp.com",
+  databaseURL: "https://tinder-polityczny-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tinder-polityczny",
+  storageBucket: "tinder-polityczny.appspot.com",
+  messagingSenderId: "664655285531",
+  appId: "1:664655285531:web:28a79cf94b58121c4797be",
+  measurementId: "G-RSDFKQM8BF",
+  
+};
+
 
 const Test = () => {
   const cards = [
     {
       id: 0,
-      text: 'Ustawa 1',
-      color: '#55ccff'
+      text: 'DUPA 1'
+      
     },
+    // {
+    //   id:2,
+    //   text:'dupa2'
+    // }
   ];
-  
+
+
+  const db = getDatabase();
+  const odwolanie= ref(db,"Pytania")
+
+
+  function dupa1(klucze){
+    const cards=[]
+    for(let i =0;i<klucze.length;i++){
+        cards.push({id:i ,text:klucze[i] })
+      }
+      console.log(cards)
+    
+  }
+
+  // onValue(odwolanie,(snapshot)=>{
+  //   const dane= Object.values(snapshot.val());
+  //   const klucze= Object.keys(snapshot.val());
+
+    
+  //   // console.log(dane)
+  //   // console.log(klucze)
+  //   renderlist = klucze.map((item,index)=>
+  //   <p key={index}>{item}</p>)  
+  //   dupa1(klucze)
+    
+  // })
+
+get(child(ref(db),'Pytania')).then((snapshot) => {
+    if (snapshot.exists()) {
+      return(dupa1(Object.keys(snapshot.val())))
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+
+
+
+
   return (
+
+    
     <div className='App'>
 
-      {cards.map((card) => (
-        <Card text={card.text} color={card.color} key={card.id}></Card>
+      {
+      cards.map((card) => (
+        <Card text={card.text} color={'#55ccff'} key={card.id}></Card>
       ))}
+    
+
     </div>
   );
 }
