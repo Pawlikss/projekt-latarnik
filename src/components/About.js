751ae138200ccfebbe0b92c72919664128a5ecda
Pawliks <pawliks.ph@gmail.com>
2022-10-23 20:07:38 +0200

    change styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,20 @@
 const About = () => {
     return (
         <article>
-            <h1 class="mainh1">Jak działa aplikacja?</h1>
+            <h2 className="mainh1">Jak działa aplikacja?</h2>
             <div className="flexBox">
 
                 <div className="flexElement">
-                    <p>
-
-                        <h3 className="title">Przygotowujemy zestaw pytań</h3>
-                        <span>Wyraź swoje zdanie na temat kwestii politycznych, możesz przerwać w każdej chwili.</span>
-                    </p>
+                    <h3 className="title">Przygotowujemy zestaw pytań</h3>
+                    <span>Wyraź swoje zdanie na temat kwestii politycznych, możesz przerwać w każdej chwili.</span>
                 </div>
                 <div className="flexElement">
-                    <p>
-                        <h3 className="title">Porównujemy twoje poglądy</h3>
-                        <span> Dzięki danym publicznym możemy porównać twoje odpowiedzi do odpowiedzi posłów.</span>
-                    </p>
+                    <h3 className="title">Porównujemy twoje poglądy</h3>
+                    <span> Dzięki danym publicznym możemy porównać twoje odpowiedzi do odpowiedzi posłów.</span>
                 </div>
                 <div className="flexElement">
-                    <p>
-                        <h3 className="title"> Wyświetlamy twoje dopasowania</h3>
-                        <span> Sprawdź do jakich polityków zbliżone są twoje poglądy.</span>
-                    </p>
+                    <h3 className="title"> Wyświetlamy twoje dopasowania</h3>
+                    <span> Sprawdź do jakich polityków zbliżone są twoje poglądy.</span>
                 </div>
             </div>
         </article>
--- src/components/MainArticle.js
+++ src/components/MainArticle.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const MainArticle = () => {
     return (
         <article className="center">
-            <h2>Wypełnij test i porównaj swoje poglądy</h2>
+            <h1>Wypełnij test i porównaj swoje poglądy</h1>
             <p>Sprawdź poglądy i przekonaj się, do kogo jest Ci najbliżej w wyborach parlamentarnych 2023.</p>
             <Link to="/test"><button className="button1">Wypełnij test</button></Link><br />
             <img src="seo_illustrations_7.png" alt="Ilustracja monitora" width="20%" height="20%" />
